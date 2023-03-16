bda23736f754ece1df9ad15b7dae7a7a087e6dd0
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-03-16 15:15:32 -0700

    Commit for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  *main - Entry point
  *Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	puts("\n"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
