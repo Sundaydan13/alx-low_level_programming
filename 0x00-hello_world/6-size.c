f56ef34c16831e42f7769fa0d1bd575943b2bee2
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-03-16 19:05:05 -0700

    Commit for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 /**
 *main - Entry point
 *Return: Always 0 (Success)
+*Printf - F is a file to be printed
 */
 int main(void)
 {
@@ -11,10 +12,10 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
