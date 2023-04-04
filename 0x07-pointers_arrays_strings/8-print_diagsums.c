2f442b580ef23fb269214a9a62c3abc807aa5fd5
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-04 16:01:36 -0700

    Commit for 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Starting point
+ * @a: get in
+ * @size: get in
+ * Return: 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int num1, num2, i;
+
+	num1 = 0;
+	num2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		num1 = num1 + a[i * size + i];
+	}
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		num2 += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", num1, num2);
+}
