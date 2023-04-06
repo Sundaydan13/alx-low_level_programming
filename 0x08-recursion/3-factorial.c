4b2ce72e83286bede9e80ecec2e4c08ede43331b
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-05 20:01:52 -0700

    Commit for 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function that returns the factorial of a given number
+ *
+ * Return: 0 (success).
+ */
+int factorial(int n);
+{
+	int n;
+	if (n==0);
+	{
+		_putchar(1);
+		_putchar(n-1);
+	}
+	else if (n<0);
+	{
+		-putchar(-1);
+	}
+	return (n);
+}
