6cc6711f14955b2f43e125b9fae0352c2c0ab329
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-10 18:08:32 -0700

    Commit for 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,40 +1,19 @@
-#include <stdio.h>
-		
+#include <stdio.h>		
 #include "main.h"
-
 		
 /**
-		
  * main - print all received arguments
-		
  * @argc: number of arguments
-		
- * @argv: array of arguments
-		
+ * @argv: array of arguments		
  *
-		
- * Return: Always 0 (Success)
-		
+ * Return: Always 0 (Success)		
  */
-		
 int main(int argc, char *argv[])
-
 {
-		
-	int a;
-		
-
-		
-	for (a = 0; a < argc; a++)
-		
+	int a;		
+	for (a = 0; a < argc; a++)	
 	{
-		
-		printf("%s\n", argv[a]);
-		
-	}
-		
-
-		
-	return (0);
-		
+		printf("%s\n", argv[a]);	
+	}	
+	return (0);	
 }
