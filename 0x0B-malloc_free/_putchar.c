9d3a8642066065d9aa165f557e91c5abfd3c7f4b
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-13 02:27:31 -0700

    Commit for 0-create_array.c

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+		
+#include <unistd.h>
+		
+/**
+		
+ * _putchar - character c to stdout
+		
+ * @c: The character to print
+		
+ *
+		
+ * Return: 1  On success
+		
+ * On error, -1 is returned, and errno is set appropriately.
+		
+ */
+		
+int _putchar(char c)
+		
+{
+		
+	return (write(1, &c, 1));
+		
+}
