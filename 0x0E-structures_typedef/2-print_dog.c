2c5974ec595db302df13db68df96aae6b511ce17
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-17 19:46:04 -0700

    Commit for 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,17 +7,17 @@
 	 * print_dog - prints a struct dog
 	 * @d: struct dog to print
 	 */
-	void print_dog(struct dog *doggy)
+	void print_dog(struct dog *d)
 	{
-		if (doggy == NULL)
+		if (d == NULL)
 			return;
 
 
-		if (doggy->name == NULL)
-			doggy->name = "(nil)";
-		if (doggy->owner == NULL)
-			doggy->owner = "(nil)";
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 
 
-		printf("Name: %s\nAge: %f\nOwner: %s\n", doggy->name, doggy->age, doggy->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
