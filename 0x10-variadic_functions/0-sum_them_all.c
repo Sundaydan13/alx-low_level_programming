ff7a3ce93b8c232abd1589ab5f3c65b1ee977385
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-20 17:37:47 -0700

    Commit for 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - function that return all the sum of the parameter
+ *
+ * Return: return 0 always on success
+ */
+
+int main(voi)
+{
+	int sum;
+	sum = n(4, 6, 12, 16);
+
+	if (n != 0)
+	{
+		printsum("%d\n", sum);
+	}
+	else
+	{
+		return(0);
+	}
+}
