5f6ebd4a9d1f25bcd6e3e21af0f6bf756ac9792c
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-20 18:50:35 -0700

    Commit for 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,20 @@
  */
 
 int sum_them_all(const unsigned int n, ...)
-{
-	int sum;
+	{
+		va_list ap;
+		unsigned int i, sum = 0;
 
-	unsigned int i, sum = 0;
-	
-	va_start(ap, n);
 
-	if (i = 0; i < n; i++);
-	
-	sum += va_arg(ap, int);
+		va_start(ap, n);
+
+
+		for (i = 0; i < n; i++)
+			sum += va_arg(ap, int);
 
 
 		va_end(ap);
 
 
-		return(sum);
-}
+		return (sum);
+	}
