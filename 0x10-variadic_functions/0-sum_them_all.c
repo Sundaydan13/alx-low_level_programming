d6e081b98be3393baf7c6ecbc7aa502705b066a4
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-04-20 18:44:10 -0700

    Commit for 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,21 +4,24 @@
 /**
  * main - function that return all the sum of the parameter
  *
- * Return: if n == 0 always on success
+ * Return: if n==0 always on success
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
-	sum = n;
-	n = (4, 6, 12, 16);
 
-	if (n != 0)
-	{
-		_putchar_sum("%d\n", sum);
-	}
-	else
-	{
-		return(0);
-	}
+	unsigned int i, sum = 0;
+	
+	va_start(ap, n);
+
+	if (i = 0; i < n; i++);
+	
+	sum += va_arg(ap, int);
+
+
+		va_end(ap);
+
+
+		return(sum);
 }
