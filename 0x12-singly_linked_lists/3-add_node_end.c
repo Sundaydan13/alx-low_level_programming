98b09c6e36857e392979047d46ff74ae81c6755f
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-05-01 03:04:16 -0700

    Commit for 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,35 +11,29 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+	
+	while (str[len])
+		len++;
 
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->len = strlen(str);
-
-	new_node->next = (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new_node;
-		return (new_node);
+		*head = new;
+		return (new);
 	}
-
-	list_t *temp = *head;
-
-	while (temp->next != NULL)
+	while (temp->next)
 		temp = temp->next;
-
-	temp->next = new_node;
-
-	return (new_node);
+	temp->next = new;
+	
+	return (new);
 }
