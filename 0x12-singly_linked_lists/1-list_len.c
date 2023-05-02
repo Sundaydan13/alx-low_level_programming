03ba9f3d9b123b19dc7c94ac8dfe5231b06cf2ac
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-05-02 04:47:38 -0700

    Commit for 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -15,7 +14,7 @@ size_t list_len(const list_t *h);
 	while (h)
 	{
 		n++;
-		h->next = h;
+		h = h->next;
 	}
 	return (n);
 }
