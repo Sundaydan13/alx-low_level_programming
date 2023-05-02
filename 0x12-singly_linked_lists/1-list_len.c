1db4d5d74616c1ca8c26bbae531d5d282bc380bc
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-05-02 04:56:25 -0700

    Commit for 3-add_node_end.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -11,12 +10,12 @@
  */
 size_t list_len(const list_t *h);
 {
-	size_t n = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-		n++;
-		h = h->next;
+		s++;
+		h->next = h;
 	}
-	return (n);
+	return (s);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,6 +34,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
-	
+
 	return (new);
 }
