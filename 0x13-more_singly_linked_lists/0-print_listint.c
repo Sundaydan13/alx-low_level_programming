9d818f93554e31b43bb41ed807c7e796427cdcd0
Sundaydan13 <ogwuchesundaydan@gmail.com>
2023-05-03 05:56:14 -0700

    Commit for 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,8 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
-	for (; h != NULL; h = h->next, num++)
+	for (; h != NULL;
+			h = h->next, num++)
 		printf("%d\n", h->n);
 
 	return (num);
